a49e55d20782e74d030c6fc375faa2504994cee8
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-08 11:47:03 +0530

    feat: added favicon

--- src/components/home/clients/Testimonials.jsx
+++ src/components/home/clients/Testimonials.jsx
@@ -17,7 +17,7 @@ const Testimonials = ( props ) => {
         <div className="space-y-10 bg-mojo_zinc px-4 py-10 md:p-20">
             <p className="text-xl md:text-2xl font-gt_america_light text-left">“{ review }”</p>
             <div className="flex flex-col md:flex-row gap-2 md:gap-10 md:items-center">
-                <div className="w-40 aspect-video bg-zinc-200">
+                <div className="w-40 aspect-video">
                     <img
                         src={ CDN_LINK + src + "?quality=" + IMAGE_QUALITY }
                         alt={ alt }
